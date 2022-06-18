bc5d4f6f1cd1e4038d19698b55c179b6e4a43357
sachin sunny <53714757+SachinDevelopment@users.noreply.github.com>
2022-06-18 18:58:42 -0400

    header

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{jsx,ts,tsx,js}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
